904e26cac1cb3f67a3d9a258aae89a59c2869d54
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2025-09-03 23:35:03 -0500

    catkin_make

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -146,6 +146,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/rodrigomaguinat/robot02_ws/build/gtest/cmake_install.cmake")
+  include("/home/rodrigomaguinat/robot02_ws/build/simple_arm/cmake_install.cmake")
 
 endif()
 
