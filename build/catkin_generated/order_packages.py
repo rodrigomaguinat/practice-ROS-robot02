33d12d868d0f4e211be3b2773bbba76ecea7ddea
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2025-09-03 23:18:31 -0500

    catkin_make

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rodrigomaguinat/robot02_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/rodrigomaguinat/robot01_ws/devel;/home/rodrigomaguinat/pit_ws/devel;/home/rodrigomaguinat/test_ws/devel;/opt/ros/noetic'.split(';') if '/home/rodrigomaguinat/robot01_ws/devel;/home/rodrigomaguinat/pit_ws/devel;/home/rodrigomaguinat/test_ws/devel;/opt/ros/noetic' != '' else []
